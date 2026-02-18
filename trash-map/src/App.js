aef68b83269215891e1bb1c4739b2e39883a483d
AK47 <adni.kazaferi@gmail.com>
2026-02-18 12:42:19 +0000

    Add Google Maps integration with MapComponent

--- trash-map/src/App.js
+++ trash-map/src/App.js
@@ -1,13 +1,15 @@
- import React from "react";
+import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
+import MapComponent from './components/MapComponent';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <MapComponent />
       <Footer />
     </div>
   );
